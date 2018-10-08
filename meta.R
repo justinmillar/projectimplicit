c239e8ff883f7be5e4b4c14cada71ee4c2f8fb95
Liz Redford <lizzie.redford@gmail.com>
2018-10-08 10:43:18 -0500

    add files

--- meta.R
+++ meta.R
@@ -4,27 +4,22 @@ library(roxygen2)
 library(testthat)
 library(knitr)
 
-### Creating built-in dataset ##### -----
-
-# Create mini sample dataset to build into package
-# Start with clean N = 8000 dataset
-raceiatdat <- haven::read_sav(file = "https://github.com/lizredford/explore-iat/blob/master/raceiat_N7983.sav?raw=true")
-raceiatdat <- raceiatdat[sample(1:nrow(raceiatdat), 100, replace = FALSE),] # Take random sample of 100 rows
-
-library(readr)
-write_csv(raceiatdat, "raceiatdat.csv") # Export data to put in data directory
-saveRDS(raceiatdat, file = "raceiatdat.RData")
-
 # Document/create NAMESPACE and .Rd files -----
 # use Roxygen2 (or really the document function in the devtools package) to process those comments and create the NAMESPACE and .Rd files.
 
 devtools::document()
 
-requireNamespace(x, quietly = TRUE)
+# Build package
+devtools::build()
+devtools::install()
+
+# Misc unused notes -----
 
 # Add packages to the imports field
-devtools::use_package("dplyr")
+# devtools::use_package("dplyr")
 # devtools::load_all() which automatically sources all files in R/
+
+requireNamespace(x, quietly = TRUE)
 
 # Some useful keyboard shortcuts for package authoring:
 #
